7e12340953075b2ce7c9af0af73a08cc4ec5bf39
apo <alexey.petrov@opencascade.com>
2017-06-15 17:04:08 +0300

    AppVeyor integration

--- build.ps1
+++ build.ps1
@@ -10,10 +10,6 @@ Import-VisualStudioVars -VisualStudioVersion $vsver -Architecture $arch
 
 Set-Location $PSScriptRoot
 
-Set-PSDebug -Trace 1
-
 Start-Process -Wait nmake "-f ${PSScriptRoot}\win32\Makefile.msc all"
-
-Set-PSDebug -Off
 
 Pop-EnvironmentBlock # Pushed by Import-VisualStudioVars
--- test.ps1
+++ test.ps1
@@ -10,10 +10,6 @@ Import-VisualStudioVars -VisualStudioVersion $vsver -Architecture $arch
 
 Set-Location $PSScriptRoot
 
-Set-PSDebug -Trace 1
-
 Start-Process -NoNewWindow -Wait nmake "-f ${PSScriptRoot}\win32\Makefile.msc test testdll"
-
-Set-PSDebug -Off
 
 Pop-EnvironmentBlock # Pushed by Import-VisualStudioVars
